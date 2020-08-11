0b0eadd68d69573793f65df80bffa6be0fa0486c
Luís Antônio <codenads@outlook.com>
2020-08-11 17:38:41 -0300

    Backend da aplicação feito

---
+++ backend/src/controllers/UserController.ts
@@ -0,0 +1,70 @@
+import { Request, Response } from 'express';
+import knex from '../database/connection';
+
+class UserController {
+    async index(request: Request, response: Response) {
+        const users = await knex('users').select('*');
+
+        if(!users) return response.json({message: "Não há usuários cadastrados."});
+
+        return response.json(users);
+
+    }
+
+    async show(request: Request, response: Response) {
+        const { id } = request.params;
+
+        const user = await knex('users').where('id', id).first();
+
+        if(!user) return response.json({message: "Não há um usuário com esse ID."});
+
+        return response.json(user);
+
+    }
+
+    async create(request: Request, response: Response) {
+        const { name, email, phone } = request.body;
+
+        const user = {
+            name,
+            email,
+            phone
+        };
+
+        const [id] = await knex('users').insert(user);
+
+        return response.json({id, ...user});
+
+    }
+
+    async update(request: Request, response: Response) {
+        const { id } = request.params;
+
+        const user = await knex('users').where('id', id).first();
+
+        const { name = user.name, email = user.email, phone = user.phone } = request.body;
+
+        const updatedRows = await knex('users').where('id', id).first().update({
+            name,
+            email,
+            phone
+        });
+
+        if(!updatedRows) return response.json({message: "Não há um usuário com esse ID."});
+
+        return response.json({message: "Usuário atualizado com sucesso"});
+
+    }
+
+    async delete(request: Request, response: Response) {
+        const { id } = request.params;
+
+        const deletedRow = await knex('users').where('id', id).first().delete();
+
+        if(!deletedRow) return response.json({message: "Não há usuário com esse ID."});
+
+        return response.json({message: "Usuário deletado com sucesso"});
+    }
+};
+
+export default UserController;
